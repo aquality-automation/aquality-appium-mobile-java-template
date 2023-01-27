9d2c1b5d35241f76066a61038120f67f4afded84
Aleksey2 Meleshko <a2.meleshko@a1qa.com>
2023-01-27 17:57:06 +0100

    add RunAppiumServer.sh and test-with-allure-report.yml workflow

---
+++ scripts/RunAppiumServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+npm install -g appium@next
+appium -v
+appium driver install xcuitest
+appium driver install uiautomator2
+appium driver list
+appium --allow-insecure chromedriver_autodownload --base-path /wd/hub &
