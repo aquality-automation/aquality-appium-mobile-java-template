e13b39df583974d2fb470f73f4e658e4f28a14f0
Aleksey2 Meleshko <a2.meleshko@a1qa.com>
2023-01-30 19:00:26 +0100

    [GitHub actions] Change devices, capabilities, timeouts and workflow pipeline

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -4,5 +4,7 @@ npm install -g appium@next
 appium -v
 appium driver install xcuitest
 appium driver install uiautomator2
+sudo /usr/bin/xcode-select -switch /Applications/Xcode.app/Contents/Developer
+xcrun --sdk iphonesimulator --show-sdk-version
 appium driver list
-appium --allow-insecure chromedriver_autodownload --base-path /wd/hub &
+appium --allow-insecure chromedriver_autodownload --base-path /wd/hub --backend-retries 5 &
