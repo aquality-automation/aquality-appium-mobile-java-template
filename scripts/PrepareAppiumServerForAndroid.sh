e85a9a9d2a9504d8bb4c0ca47ac252cb9c77a911
Alaksiej Mialeška <mialeska@users.noreply.github.com>
2026-01-29 20:10:55 +0100

    Update plugins and dependencies to latest compatible versions (#27)
    * Update plugins and dependencies to latest compatible versions
    - Update license year
    - Update appium installation scripts
    - Update settings and pipeline to use appium3
    - Update android emulator and ios simulator versions
    - use macOS-14 in azure pipelines and in github actions
    
    * - fix Android SDK configuration steps
    - set proper iOS simulator OS version
    - use Java 17 in GH actions for android sdkmanager
    
    * - setup sonarqube project
    - use macos-14-large image in GH actions
    - fix review issues and assess sonarqube issues
    
    * - use macos-12 image and java 11 in GH actions
    - reorder/reworked steps for Android and iOS configurations in ADO
    
    * - use macos-14 image and java 17 in GH actions, arm64-v8a arch for android emulator
    - add more logging to ios sim setup id ADO
    
    * - use macos-14 image and java 11 in GH actions, linux-latest image for android emulator
    - use ios 17.0 simulator in ADO, add emulator start args -no-snapshot-save -no-window -camera-back none
    
    * - use java 17 in GH actions for android emulator
    
    * - use ios 17.0 in GH actions
    
    * - use XCODE v 15.0.1 in Azure pipeline for iOS
    
    * - Create a fresh simulator in Azure pipeline for iOS
    
    * - Erase then boot simulator in Azure pipeline for iOS
    
    * - Fail fast if the simulator UID isn't resolved and ensure a blocking wait.
    - Cache key should include all AVD configuration parameters.
    
    * - Create a fresh simulator in Azure pipeline for iOS
    
    * - fix SIM_UID after creation
    
    * - fix IOS_VERSION_TYPE
    
    * - fix IOS_VERSION_TYPE
    
    * - revert to erasing device then boot ${IOS_SIM_ID} simulator"
    - Resetting CoreSimulator services and retry

--- scripts/PrepareAppiumServerForAndroid.sh
+++ scripts/PrepareAppiumServerForAndroid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-npm install -g appium@next
+npm install --ignore-scripts -g appium@3
 appium -v
 appium driver install uiautomator2
 appium driver list
-appium --allow-insecure chromedriver_autodownload --base-path /wd/hub &
+appium --allow-insecure uiautomator2:chromedriver_autodownload --base-path /wd/hub &
--- scripts/PrepareAppiumServerForIos.sh
+++ scripts/PrepareAppiumServerForIos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
-npm install -g appium@next
+npm install --ignore-scripts -g appium@3
 appium -v
 appium driver install xcuitest
 sudo /usr/bin/xcode-select -switch /Applications/Xcode.app/Contents/Developer
 xcrun --sdk iphonesimulator --show-sdk-version
 appium driver list
-appium --allow-insecure chromedriver_autodownload --base-path /wd/hub &
+appium --allow-insecure uiautomator2:chromedriver_autodownload --base-path /wd/hub &
